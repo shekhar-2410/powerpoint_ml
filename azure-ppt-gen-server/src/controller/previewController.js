379e61cd06138cbff32c68f98bc0e561dbdb4c78
shekhar <shekhar.devanshee@gmail.com>
2025-05-05 15:55:32 +0530

    table rows fixed

--- azure-ppt-gen-server/src/controller/previewController.js
+++ azure-ppt-gen-server/src/controller/previewController.js
@@ -90,9 +90,9 @@ export const generatePreview = async (req, res) => {
       "table": {
         "headers": ["Header 1", "Header 2", "Header 3",
         "rows": [
-          ["Row1-Col1", "Row1-Col2", "Row1-Col3", "Row1-Col4"],
-          ["Row2-Col1", "Row2-Col2", "Row2-Col3", "Row2-Col4"],
-          ["Row3-Col1", "Row3-Col2", "Row3-Col3", "Row3-Col4"],
+          ["Row1-Col1", "Row1-Col2", "Row1-Col3"],
+          ["Row2-Col1", "Row2-Col2", "Row2-Col3"],
+          ["Row3-Col1", "Row3-Col2", "Row3-Col3"],
         
         
         ]
